14b555822418c4b51e707331099a8654df0bdaee
Priscila Carvalho <priscila_silvacarvalho@hotmail.com>
2014-06-06 21:52:51 -0300

    Criado controllers de AssuntoController.php e VotarController.php

---
+++ application/modules/default/controllers/VotarController.php
@@ -0,0 +1,19 @@
+<?php
+class VotarController extends Zend_Controller_Action {
+	
+	// model a ser utilizado por todos os actions
+	protected $model;
+	
+	// é executada em todos os actions
+	public function init() {
+
+	}
+	
+	// criando index action http://localhost ou http://localhost/index ou http://localhost/index/index
+	public function indexAction() {
+	    $model_tema = new Application_Model_ViewTemaVotos();
+	    $temas = $model_tema->getTemas();
+	    
+	    $this->view->temas = $temas;	    
+	}
+}
